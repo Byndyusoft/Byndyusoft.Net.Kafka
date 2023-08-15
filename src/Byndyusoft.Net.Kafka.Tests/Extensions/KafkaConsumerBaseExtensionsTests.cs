90db94eaeba8c0e10e311669f987857f40077684
myasnikovik <myasnikovik@gmail.com>
2023-08-15 14:12:51 +0500

    consumer's & producer's naming were tested

---
+++ src/Byndyusoft.Net.Kafka.Tests/Extensions/KafkaConsumerBaseExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Byndyusoft.Net.Kafka.Tests.Extensions;
+
+using Byndyusoft.Net.Kafka.Extensions;
+using Common.Generators;
+using FluentAssertions;
+using Xunit;
+
+public class KafkaConsumerBaseExtensionsTests
+{
+    [Fact]
+    public void BuildingConsumersGroupId()
+    {
+        //Arrange
+        const string prefix = "Byndyusoft.SomeProject.Api";
+        var consumerMock = KafkaConsumerMockGenerator
+            .Empty()
+            .WithTopic("project.entity.creation")
+            .Object;
+
+        //Act
+        var consumersGroupId = consumerMock.BuildConsumersGroupId(prefix);
+
+        //Assert
+        consumersGroupId.Should().Be("byndyusoft.some_project_api.project_entity_creation");
+    }
+}
