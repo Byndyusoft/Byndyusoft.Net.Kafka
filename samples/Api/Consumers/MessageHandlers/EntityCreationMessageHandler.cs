8ffc7bc75e13f1ac9e136d0241abd6576e3419c3
myasnikovik <myasnikovik@gmail.com>
2023-08-25 13:31:40 +0500

    EntityCreationMessageHandler use logger instead of console

--- samples/Api/Consumers/MessageHandlers/EntityCreationMessageHandler.cs
+++ samples/Api/Consumers/MessageHandlers/EntityCreationMessageHandler.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Byndyusoft.Net.Kafka.Api.Contracts;
 using KafkaFlow;
 using KafkaFlow.TypedHandler;
+using Microsoft.Extensions.Logging;
 
 public sealed class EntityCreationMessageHandler : IMessageHandler<EntityCreation>
 {
+    private readonly ILogger<EntityCreationMessageHandler> _logger;
+
+    public EntityCreationMessageHandler(ILogger<EntityCreationMessageHandler> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
     public Task Handle(IMessageContext context, EntityCreation someEvent)
     {
-        Console.WriteLine(
+        _logger.LogInformation(
             "Partition: {0} | Offset: {1} | Message: {2}",
             context.ConsumerContext.Partition,
             context.ConsumerContext.Offset,
