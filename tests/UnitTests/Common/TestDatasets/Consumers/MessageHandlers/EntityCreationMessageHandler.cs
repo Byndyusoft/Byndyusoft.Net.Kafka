a164dc9bc50e02876316a560d976feec1ad0841d
myasnikovik <myasnikovik@gmail.com>
2023-08-25 12:25:03 +0500

    test datasets were cleaned out

--- tests/UnitTests/Common/TestDatasets/Consumers/MessageHandlers/EntityCreationMessageHandler.cs
+++ tests/UnitTests/Common/TestDatasets/Consumers/MessageHandlers/EntityCreationMessageHandler.cs
@@ -1,6 +1,5 @@
 ﻿namespace Byndyusoft.Net.Kafka.Tests.Common.TestDatasets.Consumers.MessageHandlers;
 
-using System;
 using System.Threading.Tasks;
 using KafkaFlow;
 using KafkaFlow.TypedHandler;
@@ -8,14 +7,5 @@ using KafkaFlow.TypedHandler;
 public sealed class EntityCreationMessageHandler : IMessageHandler<EntityCreation>
 {
     public Task Handle(IMessageContext context, EntityCreation someEvent)
-    {
-        Console.WriteLine(
-            "Partition: {0} | Offset: {1} | Message: {2}",
-            context.ConsumerContext.Partition,
-            context.ConsumerContext.Offset,
-            someEvent.Text
-        );
-
-        return Task.FromResult(someEvent);
-    }
+        => Task.FromResult(someEvent);
 }
