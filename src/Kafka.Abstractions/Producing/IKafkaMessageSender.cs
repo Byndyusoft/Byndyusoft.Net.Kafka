d7c5729310c00134f379063613f7e0c0728f8e08
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2024-09-11 14:13:59 +0300

    Space in comments was removed

--- src/Kafka.Abstractions/Producing/IKafkaMessageSender.cs
+++ src/Kafka.Abstractions/Producing/IKafkaMessageSender.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Sends message <paramref name="message"/> with key <paramref name="messageKey"/> to Kafka by using producing profile <paramref name="producingProfileName"/>
         /// </summary>
-        /// <param name="producingProfileName"> Producing profile name</param>
+        /// <param name="producingProfileName">Producing profile name</param>
         /// <param name="messageKey">Message key</param>
         /// <param name="message">Message</param>
         Task SendAsync<TMessage>(string producingProfileName, string messageKey, TMessage message);
