f0405611d1155a4ed71a70cc5c68798431c43b8b
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2025-11-03 20:26:19 +0300

    Debugging commands were added to the script

--- samples/start.sh
+++ samples/start.sh
@@ -1,3 +1,4 @@
 docker compose up -d
+docker container ls
 docker exec kafka kafka-configs --bootstrap-server kafka:29092 --alter --add-config 'SCRAM-SHA-512=[password=broker]' --entity-type users --entity-name broker
 docker exec kafka kafka-topics --bootstrap-server kafka:29092 --create --topic composer_assistant.entity.creation --partitions 9 --replication-factor 1
