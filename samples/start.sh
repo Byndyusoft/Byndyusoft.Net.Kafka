36d1e8df6fbf8a5a75db2ca6560e1dc1ebe1b174
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2025-11-03 20:30:45 +0300

    Logs from Kafka container were requested

--- samples/start.sh
+++ samples/start.sh
@@ -1,4 +1,5 @@
 docker compose up -d
 docker container ls
+docker logs kafka
 docker exec kafka kafka-configs --bootstrap-server kafka:29092 --alter --add-config 'SCRAM-SHA-512=[password=broker]' --entity-type users --entity-name broker
 docker exec kafka kafka-topics --bootstrap-server kafka:29092 --create --topic composer_assistant.entity.creation --partitions 9 --replication-factor 1
