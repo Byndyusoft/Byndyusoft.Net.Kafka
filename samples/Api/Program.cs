d267b2808cad3d864302c2c11bcd5dec1a3945ec
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2025-11-04 22:06:33 +0300

    Program and Startup were merged

--- samples/Api/Program.cs
+++ samples/Api/Program.cs
@@ -1,35 +1,65 @@
-namespace MusicalityLabs.ComposerAssistant.Storage.Api
+namespace MusicalityLabs.ComposerAssistant.Storage.Api;
+
+using Byndyusoft.Logging.Builders;
+using Byndyusoft.Logging.Configuration;
+using Byndyusoft.Net.Kafka;
+using Byndyusoft.Net.Kafka.Configuration;
+using Infrastructure.OpenTelemetry;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Configuration;
+
+public class Program
 {
-    using Byndyusoft.Logging.Builders;
-    using Byndyusoft.Logging.Configuration;
-    using Byndyusoft.Net.Kafka.Configuration;
-    using Infrastructure.OpenTelemetry;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Hosting;
-    using Serilog;
-
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args)
-                .Build()
-                .StartKafkaProcessing()
-                .Run();
-        }
+        var builder = WebApplication.CreateBuilder(args);
 
-        private static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var serviceName = typeof(Program).Assembly.GetName().Name!;
-            return Host.CreateDefaultBuilder(args)
-                .UseSerilog(
-                    (context, configuration) => configuration
-                        .UseDefaultSettings(context.Configuration)
-                        .UseOpenTelemetryTraces()
-                        .WriteToOpenTelemetry(activityEventBuilder: StructuredActivityEventBuilder.Instance)
-                )
-                .ConfigureServices((context, services) => services.AddOpenTelemetry(serviceName, context.Configuration))
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
-        }
+        builder.Host
+            .UseSerilog(
+                (context, configuration) => configuration
+                    .UseDefaultSettings(context.Configuration)
+                    .UseOpenTelemetryTraces()
+                    .WriteToOpenTelemetry(activityEventBuilder: StructuredActivityEventBuilder.Instance)
+                    .Enrich.WithPropertyDataAccessor()
+                    .Enrich.WithStaticTelemetryItems()
+            );
+
+        var serviceName = typeof(Program).Assembly.GetName().Name!;
+        builder.Services
+            .AddOpenTelemetry(serviceName, builder.Configuration);
+
+        builder.Services
+            .AddControllers()
+            .AddOpenTelemetryTracing();
+
+        builder.Services
+            .AddSwaggerGen()
+            .AddKafkaBus(builder.Configuration.GetSection(nameof(KafkaSettings)).Get<KafkaSettings>()!);
+
+        builder.Services
+            .ConfigureStaticTelemetryItemCollector()
+            .WithBuildConfiguration()
+            .WithAspNetCoreEnvironment()
+            .WithServiceName(serviceName);
+        
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+            app
+                .UseSwagger()
+                .UseSwaggerUI();
+        app
+            .UseHttpsRedirection()
+            .UseRouting();
+
+        app.MapControllers();
+
+        app
+            .StartKafkaProcessing()
+            .Run();
     }
 }
